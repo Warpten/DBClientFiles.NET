e7b00f06d8a672108e3d3990bc11d32b404b6625
Warpten <vertozor@gmail.com>
2018-06-01 14:14:30 +0200

    Trying out BenchmarkDotNet.

---
+++ DBClientFiles.NET.Benchmark/BenchmarkConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBClientFiles.NET.Benchmark
+{
+    public class BenchmarkConfiguration : ManualConfig
+    {
+        public BenchmarkConfiguration()
+        {
+            Add(Job.Default.With(Platform.X64).With(CsProjClassicNetToolchain.Net471));
+            Add(Job.Default.With(Platform.X64).With(CsProjCoreToolchain.NetCoreApp21));
+
+            Add(MemoryDiagnoser.Default);
+            Add(new MinimalColumnProvider());
+            Add(MemoryDiagnoser.Default.GetColumnProvider());
+            Set(new DefaultOrderProvider(SummaryOrderPolicy.SlowestToFastest));
+            Add(MarkdownExporter.GitHub);
+            Add(new ConsoleLogger());
+        }
+
+        private sealed class MinimalColumnProvider : IColumnProvider
+        {
+            public IEnumerable<IColumn> GetColumns(Summary summary)
+            {
+                yield return TargetMethodColumn.Method;
+                yield return new JobCharacteristicColumn(InfrastructureMode.ToolchainCharacteristic);
+                yield return StatisticColumn.Min;
+                yield return StatisticColumn.Max;
+                yield return StatisticColumn.Median;
+                yield return StatisticColumn.Mean;
+                yield return StatisticColumn.StdDev;
+                yield return StatisticColumn.StdErr;
+            }
+        }
+    }
+}
---
+++ DBClientFiles.NET.Benchmark/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+using DBClientFiles.NET.Collections.Generic;
+using WDBC = DBClientFiles.NET.Data.WDBC;
+using WDB2 = DBClientFiles.NET.Data.WDB2;
+
+namespace DBClientFiles.NET.Benchmark
+{
+    [MarkdownExporter, RPlotExporter, AsciiDocExporter]
+    [ClrJob, CoreJob]
+    [MinColumn, MaxColumn]
+    public class Test
+    {
+        public static void Main() => BenchmarkRunner.Run<Test>();
+
+        private static string PATH_ROOT = @"C:\Users\Vincent Piquet\source\repos\DBClientFiles.NET\DBClientFiles.NET.Benchmark\bin\Release\net472\Data";
+
+        [Benchmark(OperationsPerInvoke = 200, Description = "Achievement.dbc (WDBC)")]
+        public StorageList<WDBC.AchievementEntry> AchievementWDBC() => StructureTester<WDBC.AchievementEntry>($@"{PATH_ROOT}\WDBC\Achievement.dbc");
+    
+        [Benchmark(OperationsPerInvoke = 200, Description = "Item-sparse.db2 (WDB2)")]
+        public StorageList<WDB2.ItemSparseEntry> ItemSparseWDB2() => StructureTester<WDB2.ItemSparseEntry>($@"{PATH_ROOT}\WDB2\Item-sparse.db2");
+    
+        private StorageList<T> StructureTester<T>(string fileName)
+            where T : class, new()
+        {
+            using (var fileStream = File.OpenRead(fileName))
+                return new StorageList<T>(fileStream);
+        }
+    }
+}
