70a0472e733af12dcc0cb25badb13b38217fcf12
Warpten <vertozor@gmail.com>
2019-09-08 23:56:23 +0200

    Fix LimitStream to actually limit things.

---
+++ DBClientFiles.NET.Benchmark/AbstractAllocationBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using DBClientFiles.NET.Collections.Generic;
+
+namespace DBClientFiles.NET.Benchmark
+{
+    [MemoryDiagnoser]
+    public abstract class AbstractAllocationBenchmark<T> : AbstractBenchmark
+    {
+        protected IEnumerator<T> Enumerable { get; private set; }
+
+        protected AbstractAllocationBenchmark(string filepath) : base(filepath)
+        {
+
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            File.Position = 0;
+            Enumerable = new StorageEnumerable<T>(StorageOptions.Default, File).GetEnumerator();
+        }
+
+        [IterationCleanup]
+        public void IterationCleanup()
+        {
+            Enumerable.Dispose();
+        }
+    }
+}
--- DBClientFiles.NET/IO/LimitedStream.cs
+++ DBClientFiles.NET/IO/LimitedStream.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DBClientFiles.NET.IO
 {
@@ -8,9 +10,13 @@ namespace DBClientFiles.NET.IO
         public LimitedStream(Stream underlyingStream, long maxLength, bool disposing = true) : base(underlyingStream, disposing)
         {
             Length = maxLength;
+            _remainder = maxLength;
         }
 
+        public override bool CanWrite { get; } = false;
+
         public override long Length { get; }
+        private long _remainder;
 
         public override long Seek(long offset, SeekOrigin origin)
         {
@@ -33,7 +39,54 @@ namespace DBClientFiles.NET.IO
                     break;
             }
 
-            return base.Seek(offset, SeekOrigin.Begin);
+            return base.Seek(offset, origin);
         }
+
+        public override int Read(byte[] buffer, int offset, int count)
+            => Read(new Span<byte>(buffer, offset, count));
+
+        public override int Read(Span<byte> buffer)
+        {
+            if (_remainder <= 0)
+                return 0;
+
+            var readCount = base.Read(buffer.Slice(0, (int)Math.Min(buffer.Length, _remainder)));
+            _remainder -= readCount;
+            return readCount;
+        }
+
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+            => base.BeginRead(buffer, offset, (int)Math.Min(count, _remainder), callback, state);
+
+        public override int EndRead(IAsyncResult asyncResult)
+        {
+            var readCount = base.EndRead(asyncResult);
+            _remainder -= readCount;
+            return readCount;
+        }
+
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+            => base.ReadAsync(buffer, offset, (int) Math.Min(count, _remainder), cancellationToken);
+
+        public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+            => base.ReadAsync(buffer.Slice(0, (int) _remainder), cancellationToken);
+
+        public override void Write(byte[] buffer, int offset, int count)
+            => throw new NotSupportedException();
+
+        public override void Write(ReadOnlySpan<byte> buffer)
+            => throw new NotSupportedException();
+
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+            => throw new NotSupportedException();
+
+        public override void EndWrite(IAsyncResult asyncResult)
+            => throw new NotSupportedException();
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+            => throw new NotSupportedException();
+
+        public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+            => throw new NotSupportedException();
     }
 }
